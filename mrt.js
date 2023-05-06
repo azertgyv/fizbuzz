67d430052d2d820d9b8dcb1fa7157880006af86c
azertgyv <atagiyev077@gmail.com>
2023-05-06 21:38:04 +0400

    fizbuzz

---
+++ mrt.js
@@ -0,0 +1,32 @@
+// let ed = prompt('Daxil edin: ')
+// if (ed % 3 == 0 && ed % 5 == 0){
+//     console.log('FizzBuzz')
+// }
+// else if (ed % 3 == 0)
+// console.log('Fizz')
+
+// else if ( ed % 5 == 0)
+// console.log('Buzz')
+
+/*let ed = prompt('Rutbenizi daxil edin: ')
+if (ed == 'User'){
+    console.log('Xos geldin user')
+}
+if (ed == 'Admin'){
+    console.log('Admin xos geldin')
+}
+if (ed == 'Geust'){
+    console.log('Xos geldin guest')
+}
+*/
+
+/*
+let ed = +prompt('Ededi qeyd edin: ')
+if (ed % 2 == 0 && ed % 5 == 0){
+    console.log('Bu istediyim ededdir')
+}
+else if (ed){
+    console.log('Bu sehv emeliyyatdir')
+}
+*/
+
